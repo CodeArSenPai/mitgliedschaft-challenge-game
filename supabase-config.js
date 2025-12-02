61f9976b79fd141ef732c7f0951b136cda618d2d
CodeArSenPai <40718371+CodeArSenPai@users.noreply.github.com>
2025-12-02 23:05:59 +0100

    Add Supabase config

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// Supabase-Konfiguration
+const SUPABASE_URL = 'https://hjmlimmvcviixdwpxbpe.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqbWxpbW12Y3ZpaXhkd3B4YnBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2ODg5NjUsImV4cCI6MjA4MDI2NDk2NX0.41AT99blQNJ1XOQvBG25szDJnE1sy104I69Cowo-lGc'
+
+// Verfügbar machen für andere Skripte
+window.SUPABASE_URL = SUPABASE_URL
+window.SUPABASE_ANON_KEY = SUPABASE_ANON_KEY
